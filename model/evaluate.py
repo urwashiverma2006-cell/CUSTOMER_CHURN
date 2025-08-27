af739467221c9652d4bc4aa7c44aefeff50bc819
urwashiverma2006-cell <urwashiverma2006@gmail.com>
2025-08-27 11:23:16 +0530

    Update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -4,21 +4,21 @@ from sklearn.metrics import accuracy_score
 import joblib
 
 # Load the dataset
-df = pd.read_csv("C:\ml-ci-cd-pipeline2\data\WA_Fn-UseC_-Telco-Customer-Churn.csv")
+df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")  # Use relative path if running in GitHub Actions
 
 # Preprocess the dataset
-X = pd.get_dummies(df.drop(['customerID', 'Churn'], axis = 1), drop_first = 1)
+X = pd.get_dummies(df.drop(['customerID', 'Churn'], axis=1), drop_first=True)
 y = df['Churn']
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 101)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
 
-# Load the saved model 
-model= loblib.load('model/Telecome_model.pkl')
+# Load the saved model
+model = joblib.load('model/Telecome_model.pkl')
 
 # Make predictions
 y_pred = model.predict(X_test)
 
-# Evaluate the model 
+# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Model accuracy: {accuracy: .2f}')
+print(f'Model accuracy: {accuracy:.2f}')
